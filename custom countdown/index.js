eb8667c37836df1af418609e0a2b64407acf323a
Dev-Sarmad <sarmadh230@gmail.com>
2023-10-10 19:17:54 +0500

    getting form values

--- custom countdown/index.js
+++ custom countdown/index.js
@@ -7,3 +7,12 @@ const today = new Date().toISOString().split("T")[0];
 console.log(today); //2023-10-10T13:12:40.637Z inorder to get rid of time we have to trim from 'T'
 // so we use split method
 dateElement.setAttribute("min", today); // min is atttribute for minimum date so that we can select future dates.
+
+
+const updateCountDown = (e) => {
+    e.preventDefault();
+    let countTitle = e.srcElement[0].value;
+    let countDate = e.srcElement[1].value
+    console.log(countTitle, countDate)
+}
+countdownForm.addEventListener("submit", updateCountDown)
