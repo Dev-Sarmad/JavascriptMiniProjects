1e30eca91687d6786c52bbc49c9add000ef5a500
Dev-Sarmad <sarmadh230@gmail.com>
2023-10-10 18:17:41 +0500

    preventing from old dates

--- custom countdown/index.js
+++ custom countdown/index.js
@@ -1,1 +1,9 @@
-console.log("we are in");
+const inputContainer = document.getElementById("input-container");
+const countdownForm = document.getElementById("countdownForm");
+const dateElement = document.getElementById("date-picker");
+
+// setting the minimum date
+const today = new Date().toISOString().split("T")[0];
+console.log(today); //2023-10-10T13:12:40.637Z inorder to get rid of time we have to trim from 'T'
+// so we use split method
+dateElement.setAttribute("min", today); // min is atttribute for minimum date so that we can select future dates.
