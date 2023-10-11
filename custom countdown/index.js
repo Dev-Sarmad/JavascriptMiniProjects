2a11daa87909a7df6bc6c4b3749258ac176da6a4
Dev-Sarmad <sarmadh230@gmail.com>
2023-10-11 06:56:25 +0500

    settimeinterval runs clock after 1s

--- custom countdown/index.js
+++ custom countdown/index.js
@@ -3,7 +3,7 @@ const countdownForm = document.getElementById("countdownForm");
 const dateElement = document.getElementById("date-picker");
 const countDownElement = document.getElementById("countdown");
 const countDownTitle = document.getElementById("countdown-title");
-const countDownBtn = document.getElementById("countdown-btn");
+const countDownBtn = document.getElementById("countdown-button");
 let timeElements = document.querySelectorAll("span");
 
 let countTitle = "";
@@ -13,6 +13,7 @@ let second = 1000;
 let minute = second * 60;
 let hour = minute * 60;
 let day = hour * 24;
+let countdownActive;
 // setting the minimum date
 const today = new Date().toISOString().split("T")[0];
 console.log(today); //2023-10-10T13:12:40.637Z inorder to get rid of time we have to trim from 'T'
@@ -20,35 +21,50 @@ console.log(today); //2023-10-10T13:12:40.637Z inorder to get rid of time we hav
 dateElement.setAttribute("min", today); // min is atttribute for minimum date so that we can select future dates.
 
 const updateDom = () => {
-  // present date
-  let now = new Date().getTime();
-  //actual time length from the present date
-  let distance = countValue - now;
-  console.log("distance is " + distance);
-  const days = Math.floor(distance / day);
-  //if the distance % day remainder something like 1.5 then we have to / this by actual hour to get total hours same goes for others.
-  const hours = Math.floor((distance % day) / hour);
-  const minutes = Math.floor((distance % hour) / minute);
-  const seconds = Math.floor((distance % minute) / second);
-  console.log(minutes, seconds, days);
-  countDownTitle.textContent = `${countTitle}`;
-  timeElements[0].textContent = `${days}`;
-  timeElements[1].textContent = `${hours}`;
-  timeElements[2].textContent = `${minutes}`;
-  timeElements[3].textContent = `${seconds}`;
-  // hiding the input container and showing Countdown after setting the values.
-  inputContainer.hidden = true;
-  countDownElement.hidden = false;
+  countdownActive = setInterval(() => {
+    // present date
+    let now = new Date().getTime();
+    //actual time length from the present date
+    let distance = countValue - now;
+    console.log("distance is " + distance);
+    const days = Math.floor(distance / day);
+    //if the distance % day remainder something like 1.5 then we have to / this by actual hour to get total hours same goes for others.
+    const hours = Math.floor((distance % day) / hour);
+    const minutes = Math.floor((distance % hour) / minute);
+    const seconds = Math.floor((distance % minute) / second);
+    console.log(minutes, seconds, days);
+    countDownTitle.textContent = `${countTitle}`;
+    timeElements[0].textContent = `${days}`;
+    timeElements[1].textContent = `${hours}`;
+    timeElements[2].textContent = `${minutes}`;
+    timeElements[3].textContent = `${seconds}`;
+    // hiding the input container and showing Countdown after setting the values.
+    inputContainer.hidden = true;
+    countDownElement.hidden = false;
+  }, second);
 };
 
 const updateCountDown = (e) => {
   e.preventDefault();
   countTitle = e.srcElement[0].value;
   countDate = e.srcElement[1].value;
-
-  // getting the number version current date
-  countValue = new Date(countDate).getTime();
-  console.log(countTitle, countDate, countValue);
-  updateDom();
+  if (countDate === "" || countTitle === "") {
+    alert("please select all inputs");
+  } else {
+    // getting the number version current date
+    countValue = new Date(countDate).getTime();
+    // console.log(countTitle, countDate, countValue);
+    updateDom();
+  }
 };
+
+const reset = () => {
+  countDownElement.hidden = true;
+  inputContainer.hidden = false;
+  clearInterval(countdownActive);
+  countTitle = "";
+  countDate = "";
+};
+
 countdownForm.addEventListener("submit", updateCountDown);
+countDownBtn.addEventListener("click", reset);
