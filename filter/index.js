b9d2096de2f5828ad5157533d0fa8eac34bab70c
Dev-Sarmad <sarmadh230@gmail.com>
2023-12-12 23:10:06 +0500

    filtering the products by name

--- filter/index.js
+++ filter/index.js
@@ -1,7 +1,34 @@
 import { data } from "./data.js";
 
-const productsContainer =  document.querySelector(".products");
-const searchItem =  document.querySelector("#search");
-const categoriesContainer =  document.querySelector(".categories");
+const productsContainer = document.querySelector(".products");
+const searchItem = document.querySelector("#search");
+const categoriesContainer = document.querySelector(".categories");
 const priceRange = document.querySelector(".priceReange");
 const priceValue = document.querySelector(".priceValue");
+
+const displayProducts = (filteredProducts) => {
+  productsContainer.innerHTML = filteredProducts.map(
+    (product) =>
+      `
+    <div class="product">
+                    <img src=${product.img} alt="">
+                    <span class="name">${product.name}</span>
+                    <span class="priceText">$${product.price}</span>
+                </div>
+    `
+  );
+};
+displayProducts(data);
+
+// keyup is an eventistner that is responsible for taking the input value
+searchItem.addEventListener("keyup", (e) => {
+  const value = e.target.value.toLowerCase();
+  // filter out products
+  if (value) {
+    displayProducts(
+      data.filter((item) => item.name.toLowerCase().indexOf(value) !== -1)
+    );
+  } else {
+    displayProducts(data);
+  }
+});
