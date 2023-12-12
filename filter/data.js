95010198a013a383ea18fd89676fddd70791d113
Dev-Sarmad <sarmadh230@gmail.com>
2023-12-12 22:46:37 +0500

    search and filter init

---
+++ filter/data.js
@@ -0,0 +1,37 @@
+export let data = [
+  {
+    id: 1,
+    name: "Invicta Men's Pro Driver",
+    img : "https://m.media-amazon.com/images/I/71e04Q53xlL._AC_UY879_.jpg",
+    price: 174,
+    category: "Dress"
+  },
+  {
+    id: 2,
+    name: "Superocean Heritage",
+    img : "https://m.media-amazon.com/images/I/61hGDiWBU8L._AC_UY879_.jpg",
+    price: 174,
+    category: "Sports"
+  },
+  {
+    id: 3,
+    name: "Invicta Men's Pro Driver",
+    img : "https://m.media-amazon.com/images/I/91WvnZ1g40L._AC_UY879_.jpg",
+    price: 174,
+    category: "Dress"
+  },
+  {
+    id: 4,
+    name: "Casino Classic Resin Strap",
+    img : "https://m.media-amazon.com/images/I/51Nk5SEBARL._AC_UY879_.jpg",
+    price: 174,
+    category: "Luxary"
+  },
+  {
+    id: 1,
+    name: "Garmin venu Smart Watch",
+    img : "https://m.media-amazon.com/images/I/51kyjYuOZhL._AC_UY879_.jpg",
+    price: 174,
+    category: "Casual"
+  },
+];
---
+++ filter/index.js
@@ -0,0 +1,7 @@
+import { data } from "./data.js";
+
+const productsContainer =  document.querySelector(".products");
+const searchItem =  document.querySelector("#search");
+const categoriesContainer =  document.querySelector(".categories");
+const priceRange = document.querySelector(".priceReange");
+const priceValue = document.querySelector(".priceValue");
